f24e2e84b6e75560b06b6c4d682713671814d657
Andreas Philippi <anphil@users.noreply.github.com>
2017-01-12 16:07:10 +0100

    Implement skeleton for challenge introduction screen

---
+++ src/js/components/introduction.js
@@ -0,0 +1,8 @@
+(function(ns)
+{
+  ns.components = ns.components || {};
+  ns.components.Introduction = Vue.component('introduction',
+  {
+    template: '#introduction-template'
+  });
+})(window);
