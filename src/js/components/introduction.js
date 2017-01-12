27d1d72a968d3dbce1fd8ee4b2483aa16e6e88a0
Andreas Philippi <anphil@users.noreply.github.com>
2017-01-12 17:36:49 +0100

    Add example data and draw graph on introduction screen

--- src/js/components/introduction.js
+++ src/js/components/introduction.js
@@ -3,6 +3,22 @@
   ns.components = ns.components || {};
   ns.components.Introduction = Vue.component('introduction',
   {
-    template: '#introduction-template'
+    data: function()
+    {
+      return {
+        chartData:
+        {
+          series: ns.data.co2Emissions
+        },
+        chartOptions: ns.data.chartOptions
+      };
+    },
+
+    template: '#introduction-template',
+
+    mounted: function()
+    {
+      new Chartist.Line('.ct-chart', this.chartData, this.chartOptions);
+    }
   });
 })(window);
